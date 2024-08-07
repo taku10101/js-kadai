9ca560ba51d15a50acdc8f3d876c20da1fd36f3d
taku10101 <taku1011ht@gmail.com>
2024-08-07 12:24:31 +0900

    feat modal component

--- src/app/kadai6/_components/ModalComponent.tsx
+++ src/app/kadai6/_components/ModalComponent.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Button,
+  useDisclosure,
+} from "@yamada-ui/react";
 
-export const ModalComponent = () => {
-  return <div>ModalComponent</div>;
+import React from "react";
+type Props = {
+  title: string;
+  children: React.ReactNode;
+};
+export const ModalComponent = (props: Props) => {
+  const { title, children } = props;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button onClick={onOpen}>{title}Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalHeader>{title}Modal</ModalHeader>
+
+        <ModalBody>{children}</ModalBody>
+      </Modal>
+    </>
+  );
 };
